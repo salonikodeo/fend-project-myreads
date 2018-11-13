c9ecef898bcade781d9be8518f24a25d03906288
Saloni <saloni08g@gmail.com>
2018-11-13 20:17:13 +0530

    Use switch and stateless function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import './App.css'
 import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router'
 import MainPage from './components/pages/MainPage.js'
 import SearchPage from './components/pages/SearchPage.js'
+import './App.css'
 
 class BooksApp extends React.Component {
   render() {
     return (
-      <div>
+      <Switch>
         <Route exact path="/" component={ MainPage }/>
         <Route exact path="/search" component={ SearchPage }/>
-      </div>
+      </Switch>
     )
   }
 }
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,30 +1,27 @@
 import React from 'react'
-import * as BooksAPI from '../BooksAPI'
 
-class Books extends React.Component {
-  render() {
-    return(
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.books.imageLinks.thumbnail}")` }}></div>
-          <div className="book-shelf-changer">
-            <select
-              onChange={(event) => {this.props.moveShelf(this.props.books, event.target.value)}}
-              value={this.props.books.shelf || "none"}
-            >
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+const Books =  () => {
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.books.imageLinks.thumbnail}")` }}></div>
+        <div className="book-shelf-changer">
+          <select
+            onChange={(event) => {this.props.moveShelf(this.props.books, event.target.value)}}
+            value={this.props.books.shelf || "none"}
+          >
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{this.props.books.title}</div>
-        <div className="book-authors">{this.props.books.authors && this.props.books.authors[0]}</div>
       </div>
-    );
-  }
+      <div className="book-title">{this.props.books.title}</div>
+      <div className="book-authors">{this.props.books.authors && this.props.books.authors[0]}</div>
+    </div>
+  )
 }
 
 export default Books;
